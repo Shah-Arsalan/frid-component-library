226c223656e3dd3b6aaacfcbcf29063affa02823
Shah-Arsalan <shaharslan2278@gmail.com>
2022-04-07 13:06:22 +0530

    added js for active class

---
+++ document.js
@@ -0,0 +1,11 @@
+var sideContainer = document.getElementById("components");
+
+var links = sideContainer.getElementsByClassName("component-content");
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
